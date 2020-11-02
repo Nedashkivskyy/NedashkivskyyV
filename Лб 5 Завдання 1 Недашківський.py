0ae8cab926605b60b2c68f040161079364c0ef8d
Vadim Nedashkivsyy <71634939+Nedashkivskyy@users.noreply.github.com>
2020-11-02 11:57:35 +0200

    Add files via upload

---
+++ Лб 5 Завдання 1 Недашківський.py
@@ -0,0 +1,12 @@
+str=' nice  terrible'
+x=str.split()
+print(x)
+str='red yellow white black blue green'
+y=str.split()
+print(y)
+str='car motorcycle truck'
+z=str.split()
+print(z)
+import random
+print(random.choice(x)+' '+ random.choice(y)+' '+ random.choice(z))
+
